e33fdf9e7bdaa005aa47287929d26c4559d825be
shouyerenM <805724473@qq.com>
2019-03-01 19:57:10 +0800

    使用 Colaboratory 创建

---
+++ learn/learn-python-3-the-hard-way/ex2_1.py
@@ -0,0 +1,62 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "ex2-1.ipynb",
+      "version": "0.3.2",
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/shouyerenM/Python/blob/master/learn/learn-python-3-the-hard-way/ex2_1.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "metadata": {
+        "id": "HL_G_vzSFAfz",
+        "colab_type": "code",
+        "outputId": "7301b090-d53a-4190-e415-54a5a886fc24",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 53
+        }
+      },
+      "cell_type": "code",
+      "source": [
+        "# A comment, this is so you can read your program later.\n",
+        "#Anything after the # is ignored by python.\n",
+        "\n",
+        "print(\"I could have code like this.\") # and the comment after is ignored\n",
+        "\n",
+        "# You can also use a comment to \"disable\" or comment out code :\n",
+        "# ptint(\"This won't run.\")\n",
+        "\n",
+        "print(\"This will run.\")"
+      ],
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "I could have code like this.\n",
+            "This will run.\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
